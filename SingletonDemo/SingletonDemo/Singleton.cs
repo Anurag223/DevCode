1a52b75285c9c4241ca55142d5e575165278a36b
Anurag Nair <anurag.s.nair@gmail.com>
2020-10-03 23:51:34 +0530

    Singleton Demo App
    -Add Singleton instance Demo App

---
+++ SingletonDemo/SingletonDemo/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingletonDemo
+{
+    public class Singleton
+    {
+        private static int _counter = 0;
+        private static Singleton singleinstance = null;
+        private static readonly object lockobject = new object();
+        private Singleton()
+        {
+            _counter++;
+        }
+        public static Singleton GetInstance()    //Thread Safe Singleton Pattern implementation
+        {
+            if (singleinstance == null)
+            {
+                lock (lockobject)
+                {
+                    if (singleinstance == null)
+                    {
+                        singleinstance = new Singleton();
+                    }
+                }
+            }
+            Console.WriteLine("Singleton Method:" + _counter);
+            return singleinstance;
+        }
+    }
+}
